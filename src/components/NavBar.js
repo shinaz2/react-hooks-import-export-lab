48973b822b69d1fc193c716c87837e191dbbf8fc
Your Nameshinaz <123456789+odin@users.noreply.github.com>
2026-02-21 15:23:33 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
     </nav>
   );
 }
+
+export default NavBar;
